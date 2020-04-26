6929e749f8c6e95612786f430340e91066fd82a3
norimyfox <62606626+norimyfox@users.noreply.github.com>
2020-04-26 11:59:20 +0300

    lesson 11 and DZ

---
+++ lesson 11/js/DZ.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    'use strict';
+    // Вывод значения input 
+    let age = document.getElementById('age'),
+    ages = showUser.bind(age);
+        
+    function showUser(surname, name) {
+        alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+    }
+    ages('surname', 'name');
+
+});
